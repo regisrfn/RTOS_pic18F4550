d003297f7265f43a422a09e85f66d8e2def2fd0d
Regis Rufino <regis.rfnrodrigues@gmail.com>
2018-06-28 13:08:15 -0300

    Refactoring - save context

--- TASK.h
+++ TASK.h
@@ -20,70 +20,20 @@
 asm("MOVFF	WREG, _taskWREG");											\
 asm("MOVFF   STATUS, _taskSTATUS");										\
 asm("MOVFF	BSR, _taskBSR");											\
-asm("MOVFF	FSR2L, _taskFSR2L");										\
-asm("MOVFF	FSR2H, _taskFSR2H");										\
-asm("MOVFF	FSR0L, _taskFSR0L");										\
-asm("MOVFF	FSR0H, _taskFSR0H");										\
-asm("MOVFF	TABLAT, _taskTABLAT");										\
-asm("MOVFF	TBLPTRU, _taskTBLPTRU");									\
-asm("MOVFF	TBLPTRH, _taskTBLPTRH");									\
-asm("MOVFF	TBLPTRL, _taskTBLPTRL");									\
-asm("MOVFF	PRODH, _taskPRODH");										\
-asm("MOVFF	PRODL, _taskPRODL");										\
-asm("MOVFF	PCLATU, _taskPCLATU");										\
-asm("MOVFF	PCLATH, _taskPCLATH");										\
-asm("CLRF	FSR0L");													\
-asm("CLRF	FSR0H");													\
 }
 
 #define SAVE_INTO_STACK {\
-savedContext[taskNumber][0] = taskPCLATH;\
-savedContext[taskNumber][1] = taskPCLATU;\
-savedContext[taskNumber][2] = taskPRODL;\
-savedContext[taskNumber][3] = taskPRODH;\
-savedContext[taskNumber][4] = taskTBLPTRL;\
-savedContext[taskNumber][5] = taskTBLPTRH;\
-savedContext[taskNumber][6] = taskTBLPTRU;\
-savedContext[taskNumber][7] = taskTABLAT;\
-savedContext[taskNumber][8] = taskFSR0H;\
-savedContext[taskNumber][9] = taskFSR0L;\
-savedContext[taskNumber][10] = taskFSR2H;\
-savedContext[taskNumber][11] = taskFSR2L;\
 savedContext[taskNumber][12] = taskBSR;\
 savedContext[taskNumber][13] = taskSTATUS;\
 savedContext[taskNumber][14] = taskWREG;\
 }
 #define RESTORE_FROM_STACK {\
-taskPCLATH = savedContext[taskNumber][0];\
-taskPCLATU = savedContext[taskNumber][1];\
-taskPRODL = savedContext[taskNumber][2];\
-taskPRODH = savedContext[taskNumber][3];\
-taskTBLPTRL = savedContext[taskNumber][4];\
-taskTBLPTRH = savedContext[taskNumber][5];\
-taskTBLPTRU = savedContext[taskNumber][6];\
-taskTABLAT = savedContext[taskNumber][7];\
-taskFSR0H = savedContext[taskNumber][8];\
-taskFSR0L = savedContext[taskNumber][9];\
-taskFSR2H = savedContext[taskNumber][10];\
-taskFSR2L = savedContext[taskNumber][11];\
 taskBSR = savedContext[taskNumber][12];\
 taskSTATUS = savedContext[taskNumber][13];\
 taskWREG = savedContext[taskNumber][14];\
 }
 
 #define RESTORE_CONTEXT {\
-asm("MOVFF	_taskPCLATH, PCLATH");										\
-asm("MOVFF	_taskPCLATU, PCLATU");										\
-asm("MOVFF	_taskPRODL, PRODL");										\
-asm("MOVFF	_taskPRODH, PRODH");										\
-asm("MOVFF	_taskTBLPTRL, TBLPTRL");									\
-asm("MOVFF	_taskTBLPTRH, TBLPTRH");									\
-asm("MOVFF	_taskTBLPTRU, TBLPTRU");									\
-asm("MOVFF	_taskTABLAT, TABLAT");										\
-asm("MOVFF	_taskFSR0H, FSR0H");										\
-asm("MOVFF	_taskFSR0L, FSR0L");										\
-asm("MOVFF	_taskFSR2H, FSR2H");										\
-asm("MOVFF	_taskFSR2L, FSR2L");										\
 asm("MOVFF	_taskBSR, BSR");											\
 asm("MOVFF	_taskSTATUS, STATUS");										\
 asm("MOVFF	_taskWREG, WREG");											\
@@ -97,8 +47,6 @@ unsigned int  taskCountTime [NUMBER_OF_TASKS];
 unsigned char size_stack    [NUMBER_OF_TASKS];
 unsigned char taskNumber = 0;
 unsigned char taskWREG, taskSTATUS, taskBSR;
-unsigned char taskFSR2L,taskFSR2H,taskFSR0L,taskFSR0H,taskTABLAT;
-unsigned char taskTBLPTRU,taskTBLPTRH,taskTBLPTRL,taskPRODH,taskPRODL,taskPCLATU,taskPCLATH;
 
 void interrupt highPriority();
 void setTimerTasks();
--- task.c
+++ task.c
@@ -1,62 +1,74 @@
-// This is a guard condition so that contents of this file are not included
-// more than once.  
-#ifndef TASK_H
-#define	TASK_H
+/*
+ * File:   task.c
+ * Author: regis
+ *
+ * Created on 3 de Junho de 2017, 11:57
+ */
 
-#include <xc.h> // include processor files - each processor file is guarded.  
-#define MAX_TASKS 3
-#define MINIMUM_TASK_TIME 10UL //us
-#define NUMBER_OF_TASKS (MAX_TASKS+1)
 
-#define FREQ_PIC 48UL // (MHz)
-#define _XTAL_FREQ 48000000
-#define PRESCALE 1UL
-#define TIMER (65535UL - (MINIMUM_TASK_TIME*FREQ_PIC/(4*PRESCALE)))
+#include <xc.h>
+#include "TASK.h"
 
-#define MAX_SIZE_STACK 10 // maximum size of stack functions --> max functions for task
+void interrupt highPriority() {
+    SAVE_CONTEXT;
+    if (TMR1IF == 1) {
+        SAVE_INTO_STACK;
 
-#define SAVE_REGISTERS {\
-taskWREG = WREG;\
-taskSTATUS = STATUS;\
-taskBSR = BSR;\
+        size_stack[taskNumber] = 0;
+        while (STKPTR > 1) {
+            taskStack[taskNumber][0][size_stack[taskNumber]] = TOSL;
+            taskStack[taskNumber][1][size_stack[taskNumber]] = TOSH;
+            taskStack[taskNumber][2][size_stack[taskNumber]] = TOSU;
+            asm("POP");
+            size_stack[taskNumber]++;
+        }
+        size_stack[taskNumber]--;
+
+        taskNumber++;
+        if (taskNumber > NUMBER_OF_TASKS - 1)
+            taskNumber = 1;
+
+
+        while ((signed char) size_stack[taskNumber] > -1) {
+            asm("PUSH");
+            TOSL = taskStack[taskNumber][0][size_stack[taskNumber]];
+            TOSH = taskStack[taskNumber][1][size_stack[taskNumber]];
+            TOSU = taskStack[taskNumber][2][size_stack[taskNumber]];
+            size_stack[taskNumber]--;
+        }
+
+        RESTORE_FROM_STACK;
+                
+        TMR1IF = 0;
+        TMR1 = Timer1;
+        RESTORE_CONTEXT;
+        asm("retfie");
+    }
+
 }
 
-#define SAVE_CONTEXT {\
-    savedContext[taskNumber][0] = taskWREG;\
-    savedContext[taskNumber][1] = taskSTATUS;\
-    savedContext[taskNumber][2] = taskBSR;\
+void setTimerTasks() {
+    TMR1IF = 0;
+    /* Enable 16-bit TMR1 register,no pre-scale,internal clock, timer OFF */
+    T1CON = 0x80;
+    TMR1 = Timer1; /* Load Count for generating delay of 1ms */
+    TMR1ON = 1; /* Turn ON Timer1 */
+    TMR1IE = 1; /* Enable Timer1 Overflow Interrupt */
 }
 
-#define RESTORE_CONTEXT {\
-    BSR = taskBSR;\
-    WREG = taskWREG;\
-    STATUS = taskSTATUS;\
+void startRTOS(void) {
+    setTimerTasks();
+    IPEN = 1;
+    GIEH = 1;
+    GIEL = 1;
+    TMR1IF = 1;
 }
-unsigned short Timer1 = TIMER;
-unsigned char taskStack     [NUMBER_OF_TASKS][3][MAX_SIZE_STACK];
-unsigned char savedContext  [NUMBER_OF_TASKS][3];
-unsigned char taskBlocked   [NUMBER_OF_TASKS];
-unsigned int  taskTime      [NUMBER_OF_TASKS];
-unsigned int  taskCountTime [NUMBER_OF_TASKS];
-unsigned char size_stack    [NUMBER_OF_TASKS];
-unsigned char taskNumber = 0;
-unsigned char taskWREG, taskSTATUS, taskBSR;
 
-void interrupt highPriority();
-void setTimerTasks();
-void initBlocked();
-void initTask(unsigned char TaskNo, unsigned int t);
-void startRTOS();
+void initTask(unsigned char TaskNo, unsigned int t) {
 
-#ifdef	__cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
-    // TODO If C++ is being used, regular C code needs function names to have C 
-    // linkage so the functions can be used by the c code. 
-
-#ifdef	__cplusplus
+    taskStack[TaskNo][0][0] = TOSL;
+    taskStack[TaskNo][1][0] = TOSH;
+    taskStack[TaskNo][2][0] = TOSU;
+    taskTime[TaskNo] = t;
+    asm("POP");
 }
-#endif /* __cplusplus */
-
-#endif /* TASK_H */
